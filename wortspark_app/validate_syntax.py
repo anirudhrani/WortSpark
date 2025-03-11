97bad3171ba0708b0ba3e54d11db21e9c7378a5b
Anirudh Rani (Ven) <anirudhrvs@gmail.com>
2025-03-11 15:14:34 -0700

    Return Error Message Functionality added

--- wortspark_app/validate_syntax.py
+++ wortspark_app/validate_syntax.py
@@ -1,40 +1,40 @@
+from antlr4.error.ErrorListener import ErrorListener
 from antlr4 import InputStream, CommonTokenStream
 from grammar.wandelscriptLexer import wandelscriptLexer 
 from grammar.wandelscriptParser import wandelscriptParser 
-from antlr4.error.ErrorListener import ErrorListener
 
-# Custom error listener to capture syntax errors
-class MyErrorListener(ErrorListener):
+class WortSparkErrorListener(ErrorListener):
     def __init__(self):
-        super(MyErrorListener, self).__init__()
-        self.has_error = False
+        super(WortSparkErrorListener, self).__init__()
+        self.errors = []
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        self.has_error = True
+        self.errors.append(f"Line {line}, Column {column}: {msg}")
 
-def is_valid_code(code: str) -> bool:
-    # Create an input stream from the code string
+
+
+def is_valid_code(code: str) -> (bool, list):
     input_stream = InputStream(code)
-    
-    # Instantiate the lexer with the input stream
     lexer = wandelscriptLexer(input_stream)
-    
-    # Generate tokens from the lexer
     token_stream = CommonTokenStream(lexer)
-    
-    # Instantiate the parser with the token stream
     parser = wandelscriptParser(token_stream)
     
-    # Remove default error listeners and add custom listener
     parser.removeErrorListeners()
-    error_listener = MyErrorListener()
+    error_listener = WortSparkErrorListener()
     parser.addErrorListener(error_listener)
     
-    # Parse the input starting at the correct rule ('program')
-    parser.program()  
-    return not error_listener.has_error
+    parser.program()
+    
+    is_valid = len(error_listener.errors) == 0
+    return is_valid, error_listener.errors
 
-# # a trailing newline is a must
-# if __name__ == "__main__":
-#     code_snippet = """a1 = 0 and 0\n"""
-#     print(is_valid_code(code_snippet))  
+# Main
+if __name__ == "__main__":
+    code_snippet = """a1 = 0 and 
+                    k= \n"""
+    valid, errors = is_valid_code(code_snippet)
+    print("Valid code:", valid)
+    if not valid:
+        print("Errors:")
+        for err in errors:
+            print(err)
